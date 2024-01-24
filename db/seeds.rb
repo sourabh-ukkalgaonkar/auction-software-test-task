b90aa7abf0e5ec0aa3e2f49bf4f9cd1a5982595f
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2024-01-24 22:13:53 +0530

    Added the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+password = "Test@123"
+
+# Created a deault user 
+User.create(
+  name: 'tester',
+  username: 'tester',
+  email: 'tester@yopmail.com',
+  password: password
+)
+
+# Created a list of users
+(0..10).each do
+  User.create(
+    name: Faker::Name.name,
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: password,
+  )
+end
+
+# Created a list of categories
+(0..10).each do 
+  Category.create(
+    name: Faker::Commerce.department
+  )
+end
+
+# Created a list of projects
+
+users = User.all
+categories = Category.all
+
+(0..10).each do 
+  Project.create(
+    title: Faker::Book.title,
+    user_id: users.sample.id,
+    category_id: categories.sample.id
+  )
+end
