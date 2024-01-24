07710c6452ee1e5fcb657eafe3210de775dee20f
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2024-01-24 18:09:39 +0530

    Added user authentication methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,36 @@ class User < ApplicationRecord
   validates :email, format: URI::MailTo::EMAIL_REGEXP
 
   attr_accessor :password
+
+  before_save :set_secure_password
+
+  def authenticate?(password_string)
+    generate_secure_password(password_string) == secure_password
+  end
+
+  private
+
+  def set_secure_password
+    generate_unique_salt if salt.blank?
+    self.secure_password = generate_secure_password(password)
+  end
+  
+  def generate_secure_password(password_string)
+    Digest::MD5.hexdigest(
+      Digest::MD5.hexdigest(
+        "#{password_string}.#{salt}"
+      )
+    )
+  end
+
+  def generate_unique_salt
+    loop do
+      self.salt = generate_random_string
+      break unless User.exists?(salt: salt)
+    end
+  end
+
+  def generate_random_string
+    SecureRandom.hex(16)  # Adjust the length as needed
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:password) { Faker::Internet.password }
+  let(:user) { create(:user, password: password) }
+
   context 'Validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
@@ -11,5 +14,17 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:username) }
     it { should allow_value(Faker::Internet.email).for(:email) }
     it { should_not allow_value(Faker::Name.name).for(:email) }
+  end
+
+  context 'Instance Methods' do
+    describe 'authenticate?' do
+      it 'should authenticate user' do
+        expect(user.authenticate?(password)).to be_truthy
+      end
+
+      it 'should not authenticate user' do
+        expect(user.authenticate?(Faker::Internet.password)).to be_falsey
+      end
+    end
   end
 end
